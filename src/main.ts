65d1035a31473217fc9db003c1fea25c269fe515
ThomasBfrd <thomas.bfrd@gmail.com>
2026-03-11 16:06:06 +0100

    fix: ajout du configService pour lecture des variables d'environnement

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.use(cookieParser());
 
+  const allowedOrigins: Array<string | undefined> = [
+    configService.get<string>('LOCAL_CLIENT'),
+    configService.get<string>('CLIENT_PATH'),
+    configService.get<string>('BACK_PATH'),
+  ];
+
   app.enableCors({
-    origin: [
-      process.env.LOCAL_CLIENT,
-      process.env.CLIENT_PATH,
-      process.env.BACK_PATH,
-    ],
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, allow?: boolean) => void,
+    ) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
@@ -24,6 +37,8 @@ async function bootstrap() {
     exposedHeaders: ['Set-Cookie'],
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port: number | undefined = configService.get<number>('PORT');
+  await app.listen(port ?? 3000);
 }
+
 bootstrap();
